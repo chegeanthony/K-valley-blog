67c66ff64e59709f17b823046e7a327639940824
SandroKE <koomealessandro@gmail.com>
2024-07-08 13:42:27 -0600

    created Factories
    - Encountered so many migration errors while working on these file
    - Created Author and Blog factories
    - updated code on migrations (change_id_column_type)

--- database/migrations/2024_07_03_232222_change_id_column_type_in_authors_table.php
+++ database/migrations/2024_07_03_232222_change_id_column_type_in_authors_table.php
@@ -4,22 +4,42 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class ChangeIdColumnTypeInAuthorsTable extends Migration
 {
     /**
      * Run the migrations.
      */
     public function up()
     {
-        Schema::table('authors', function (Blueprint $table) {
-            $table->bigInteger('id')->change();
+        // Drop foreign key constraint in blogs table if exists
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->dropForeign(['author_id']);
         });
-    }
-    
-    public function down()
-    {
+
+        // Modify id column in authors table
         Schema::table('authors', function (Blueprint $table) {
             $table->string('id')->change();
         });
     }
-};
+    
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        // Drop author_id column in blogs table if exists
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->dropColumn('author_id');
+        });
+
+        // Revert id column in authors table
+        Schema::table('authors', function (Blueprint $table) {
+            $table->bigInteger('id')->change();
+        });
+
+        // Recreate foreign key constraint in blogs table
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->foreignId('author_id')->constrained()->onDelete('cascade');
+        });
+    }
+}
